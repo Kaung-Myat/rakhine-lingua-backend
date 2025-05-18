6b83224204abfde03417d2d0b3b9cbb099b19ceb
Kaung Myat Thu <116712118+Kaung-Myat@users.noreply.github.com>
2025-05-18 12:35:39 +0630

    Update sheetConfigs.tone_of_voice.js

--- config/sheetConfigs.tone_of_voice.js
+++ config/sheetConfigs.tone_of_voice.js
@@ -3,6 +3,21 @@ const sheetConfigs = {
         title: "အုအာ ကာရံ",
         spreadsheetId: "1Ul5AzGzRjgFKLRTOfo2fxBsjWsyrfCiPGiN33ROHViQ",
         range: "A:C",
+    },
+    AKarams: {
+        title: "အေ ကာရံ",
+        spreadsheetId: "1M_QAwfoEIfwxuHOw0948721lsbsO1zM2MDOWwFWi_Yg",
+        range: "A:C",
+    },
+    AaiKarams: {
+        title: "အည် ကာရံ",
+        spreadsheetId: "1ew9mlWPJh4MlRRi1_3ehbVrG8TxP42hRFApiyRPPjTE",
+        range: "A:C",
+    },
+    AaitKarams: {
+        title: "အစ် ကာရံ",
+        spreadsheetId: "1Mde-cFqRPhS8ImN48uePLbLRzkqduzGC9qtQ2Dwx5wo",
+        range: "A:C",
     }
     // Add more configurations as needed
 };
